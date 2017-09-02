d75614962d1b03e5df1e97653660263213c362a4
lq <r@g.com>
2017-09-02 07:54:29 -0400

    6

--- site1/src/App.js
+++ site1/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {TodoForm,TodoList} from './componets/todo'
 class App extends Component {
   constructor(){
     super()
     this.state={
       todos:[
         { id:1,name:'Learn JSX1',isComplete:true},
-        { id:2,name:'Learn JSX2',isComplete:false},
-        { id:3,name:'Learn JSX3',isComplete:false}
+        { id:2,name:'Build an Awesome App',isComplete:false},
+        { id:3,name:'Ship It',isComplete:false}
     
       ],currentTodo:''
     }
@@ -30,18 +30,9 @@ class App extends Component {
           <h2>React Todos</h2>
         </div>
           <div className="Todo-App">
-           <form>
-             <input type="text" onChange={this.handleInputChange}  value={this.state.currentTodo}/>
-           </form>
-           <div className="Todo-list">
-             <ul>
-               {this.state.todos.map(todo=><li key={todo.id}>
-                 <input type="checkbox" defaultChecked={todo.isComplete} />
-                 {todo.name}</li>)
-                 }
-
-             </ul>
-           </div>
+          <TodoForm handleInputChange={this.handleInputChange} currentTodo={this.state.currentTodo}/>
+          <TodoList todos={this.state.todos}/>
+        
           </div>
       </div>
     );
